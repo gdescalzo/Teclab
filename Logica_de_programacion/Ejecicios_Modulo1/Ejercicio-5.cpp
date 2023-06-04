0dea96b685b060c78e9333ad5afa2de0f2c5836e
Gaston <gdescalzo@gmail.com>
2023-06-04 15:36:41 -0300

    Adding Ejercicio-5

---
+++ Logica_de_programacion/Ejecicios_Modulo1/Ejercicio-5.cpp
@@ -0,0 +1,39 @@
+/*
+
+Escribir en lenguaje C un programa que calcule el perímetro de una circunferencia.
+    1. Pida por teclado el radio de una circunferencia.
+    2. Calcule el perímetro de la circunferencia.
+    3. Muestre por pantalla el resultado.
+
+    Nota: π (pi) debe declararse como una constante
+
+*/
+
+// Importamos librerias
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // Declaramos las variables
+    float radio;
+    float perimetro;
+    float pi = 3.14;
+
+    cout << "\n\n### Calculador de perimetro de una circunferencia ###\n\n";
+
+    // Validamos ingreso de usuario (radio)
+    do
+    {
+        cout << " Ingrese el radio de la circunferencia: ";
+        cin >> radio;
+
+        if (radio < 0)
+        {
+            cout << "\n Usted ingreso un numero negativo,\n porfavor ingrese numeros positivos,\n Ingrese nuevamente el radio de la circunferencia: ";
+            cin >> radio;
+        }
+    } while (radio < 0);
+    
+}
