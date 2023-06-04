e1ea3988e8d8819c39479ef36ba5cf6c70ec4e45
Gaston <gdescalzo@gmail.com>
2023-06-04 15:01:13 -0300

    Finish Ejercicio-3

--- Logica_de_programacion/Ejecicios_Modulo1/Ejercicio-3.cpp
+++ Logica_de_programacion/Ejecicios_Modulo1/Ejercicio-3.cpp
@@ -15,23 +15,38 @@ int main()
 {
     // Declaramos las variables
     float notas[3];  
-    float promedio;
+    float promedio = 0;
     int indice;
 
-    cout << "\n\n### Calculador de promedio de notas (3) ###\n\n";
+    cout << "\n\n### Calculador de promedio de notas (3) ###\n";
 
+    
+    // Ingreso de notas en un array (nota 1, 2, 3)
     for (int i = 0; i < 3; i++)
     {
-        indice = i +1;
-        cout << " Por favor ingrese la nota numero (" << indice << "): ";
-        cin >> notas[i];
+        // Validamos ingreso de usuario (notas)
+        do
+        {
+            indice = i + 1;
+            cout << "\n Por favor ingrese la nota numero (" << indice << "): ";
+            cin >> notas[i];
+
+            if (notas[i] < 0)
+            {
+                cout << "\n Usted ingreso un numero negativo,\n por favor ingrese numeros positivos\n\n por favor ingrese la nota numero (" << indice << "): ";
+                cin >> notas[i];
+            }
+        } while (notas[i] < 0);
     }
     
     cout << "\nLas notas ingresadas son: ";
     for (int p = 0; p < 3; p++)
     {
         indice = p +1;
+        promedio = promedio + notas[p];
         cout << "\n Nota ("<< indice << "): " << notas[p];
+        
     }
-    
+
+    cout << "\n\n El promedio es: " << (promedio / 3) ;
 }
