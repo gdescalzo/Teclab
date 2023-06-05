8e8a6ced0e19d0e2907b151c6e02bb4afa86f3fa
Gaston <gdescalzo@gmail.com>
2023-06-05 19:42:51 -0300

    Finish Ejercicio-3 (Modulo_3)

--- Logica_de_programacion/Ejercicios_Modulo_3/Ejercicio-3.cpp
+++ Logica_de_programacion/Ejercicios_Modulo_3/Ejercicio-3.cpp
@@ -20,15 +20,18 @@ int main()
 {
     // Declaro las variables
     int cantidad_alumnos;
-    int nota;
+    float nota;
+    float suma;
+    float aux;
 
     cout << "\n\n### Calculador del promedio y la nota más alta ###\n\n";
 
     cout << "Ingrese la cantidad de alumnos: ";
     cin >> cantidad_alumnos;
+    cout << "\n";
 
     // Creamos el array de alumnos
-    int *alumnos = new int[cantidad_alumnos];
+    float *alumnos = new int[cantidad_alumnos];
 
     // Cargamos las notas en el array
     for (int i = 1; i < cantidad_alumnos +1; i++)
@@ -52,10 +55,28 @@ int main()
         } while (alumnos[i] < 0 || alumnos[i] > 10);
     }
     
+    // Mostramos las notas ingresadas y sumamos las mismas
     for (int j = 1; j < cantidad_alumnos +1; j++)
     {
         cout << "\nLa nota ingresada para el alumno '" << j << "' es: " << alumnos[j];
+        suma = suma + alumnos[j];
+
+        // Almacenamos la nota mas alta
+        if (alumnos[j] > alumnos[j+1])
+        {
+            aux = alumnos[j];
+        }
+        else if (alumnos[j] < alumnos[j+1])
+        {
+            aux = alumnos[j+1];
+        }
     }
+
+    // Calculamos el promedio
+    float promedio = suma / cantidad_alumnos;
+
+    cout << "\n\nEl promedio es: " << promedio;
+    cout << "\n\nLa nota mas alta es: " << aux;
     
     return 0;
 }
