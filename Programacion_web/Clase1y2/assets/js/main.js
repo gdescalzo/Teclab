3acc2eefe3cbb71256545454022d3b7579aed497
Gaston Descalzo <gdescalzo@outlook.com>
2024-06-12 21:03:55 -0300

    First commit.

---
+++ Programacion_web/Clase1y2/assets/js/main.js
@@ -0,0 +1,43 @@
+var cadena = "esto es una cadena de caracteres";
+var entero = 5;
+var decimal = 4.5;
+var caracter = "C";
+var booleano = true;
+var arreglo = ["elemento1","elemento2","elemento3"];
+console.log(cadena);
+console.log(entero);
+console.log(decimal);
+console.log(caracter);
+console.log(booleano);
+console.log(arreglo);
+
+if (booleano){
+    alert("la exprecion del if es true")
+}else{
+    alert("no paso por aqui")
+}
+
+for (var i = 0; i < 10; i++){
+    console.log("el valor de i es: " + i)
+}
+
+switch(caracter){
+    case "A":
+        alert("el valor de A es");
+        break;
+    case "B":
+        alert("el valor de B es");
+        break;
+    case "C":
+        alert("el valor de C es");
+        break;
+    case "D":
+        alert("el valor de D es");
+        break;
+}
+
+var contador = 1;
+while  (contador <=5){
+    console.log("en el cliclo While: " + contador)
+    contador ++;
+}
---
+++ Programacion_web/Clase1y2/index.php
@@ -0,0 +1,53 @@
+<! DOCTYPE html>
+<html>
+    <head>
+        <title>TECLAB - PRODUCTOS</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content=""width=device-width, initial-scale=1.0>
+        <link href="./assets/css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <section style="background-color: $2780bl; text-align: center;">
+            <img src="./assets/img/teclab.png">
+        </section>
+
+        <section>
+            <h1>TITULO EL PROYECTO</h1>
+            <p>
+                Informaci&oacute;n general
+                <span> Texto resaltado</span>
+            </p>
+            <a href="https://www.google.com" target="_blank">Click aqu&iacute;</a>
+        </section>
+        <section> 
+            <center>
+                <table id="productos">
+                    <thead>
+                        <tr>
+                            <td>&nsbp;</td>
+                            <td>ID</td>
+                            <td>Categoria</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>M - E</td>
+                            <td>1</td>
+                            <td>Monitores</td>
+                        </tr>
+                        <tr class="gris">
+                            <td>M - E</td>
+                            <td>2</td>
+                            <td>Teclados</td>
+                        </tr>
+                        <tr></tr>
+                            <td>M - E</td>
+                            <td>3</td>
+                            <td>audio</td>
+                        </tr>    
+                    </tbody>
+                </table>
+            </center>
+        </section>
+    </body>
+</html>
